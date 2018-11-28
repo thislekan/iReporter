fe4d15b4a2f5ea7a6812f902a6e4eaad1be17cf9
thislekan <lekanomoniyi@yahoo.com>
2018-11-28 22:20:26 +0100

    setup server
    - create idGenerator and authorizeUser middlewares
    - create store in db directory
    - create routes
    - create userHandler and redFlagHandler controllers
    - create server.js to serve endpoints

---
+++ server/middlewares/idGenerator.js
@@ -0,0 +1,12 @@
+
+const idGenerator = () => {
+  let text = '';
+  const possible = '0123456789';
+
+  for (let i = 0; i < 5; i += 1) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+export default idGenerator;
---
+++ server/routes/routes.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+import express from 'express';
+import authorizeUser from '../middlewares/authorizeUser';
+import userHandler from '../controllers/userHandler';
+import redFlagHandler from '../controllers/redFlagHandler';
+// import parcelHandler from '../controllers/parcelHandler';
+
+const app = express.Router();
+const apiVersion = '/api/v1/';
+
+app.route(`${apiVersion}user/create`)
+  .post(authorizeUser, userHandler.createUser);
+
+app.route(`${apiVersion}user/login`)
+  .post(userHandler.loginUser);
+
+app.route(`${apiVersion}red-flags`)
+  .get(redFlagHandler.getAllRedFlags)
+  .post(redFlagHandler.createRedFlag);
+
+app.route(`${apiVersion}red-flags/:id`)
+  .get(redFlagHandler.getRedFlagById)
+  .delete(redFlagHandler.deleteRedFlag);
+
+app.route(`${apiVersion}red-flags/:id/location`)
+  .patch(redFlagHandler.editRedFlagLocation);
+
+app.route(`${apiVersion}red-flags/:id/comment`)
+  .patch(redFlagHandler.editRedFlagComment);
+
+export default app;
