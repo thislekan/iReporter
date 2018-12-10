73ae542092bef293bd0ba9a441ed33e0268295f2
Ibrahim Olalekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2018-12-10 23:35:01 +0100

    setup database
    - install packages needed for database setup
    - create database.js for instantiating tables
    - create dbMiddleware to interface queries with db

---
+++ server/models/migrations.sql
@@ -0,0 +1,29 @@
+-- DROP DATABASE IF EXISTS iReporter;
+-- CREATE DATABASE iReporter;
+
+-- \c iReporter;
+
+CREATE TABLE Users
+(
+  id SERIAL PRIMARY KEY,
+  fullname VARCHAR(60) NOT NULL,
+  lastname VARCHAR(20) NOT NULL,
+  firstname VARCHAR(20) NOT NULL,
+  othernames VARCHAR(20) NOT NULL,
+  email VARCHAR(25) NOT NULL,
+  isAdmin BOOLEAN NOT NULL default FALSE,
+  phoneNumber VARCHAR(12) NOT NULL,
+  username VARCHAR(10) NOT NULL,
+)
+
+CREATE TABLE Incident
+(
+  id SERIAL PRIMARY KEY,
+  createdOn INT NOT NULL,
+  createdBy INT NOT NULL,
+  type VARCHAR(15) NOT NULL,
+  location VARCHAR(50) NOT NULL,
+  Images VARCHAR(75),
+  Videos VARCHAR(75),
+  comment VARCHAR(150),
+)
