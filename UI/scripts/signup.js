c571323905ff0a07e8fc7d01840cc8586c5ac3ab
Ibrahim Olalekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2018-12-26 23:12:12 +0100

    task [feature/ui-templates]: change signup requirements
    - include firstname and lastname fields

--- UI/scripts/signup.js
+++ UI/scripts/signup.js
@@ -1,4 +1,5 @@
-const nameInput = document.getElementById('name');
+const firstNameInput = document.getElementById('first-lastName');
+const lastNameInput = document.getElementById('last-lastName');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 const passwordInputCheck = document.getElementById('confirm-password');
@@ -9,6 +10,7 @@ const notificationBoxCloser = document.getElementById('close-notification');
 const notificationTextElement = notificationBox.querySelector('p');
 
 const signUpButton = document.getElementById('signup-btn');
+const apiVersion = 'https://ireporter-endpoint.herokuapp.com/api/v2/';
 
 notificationBoxCloser.addEventListener('click', () => {
   notificationBox.style.display = 'none';
@@ -19,12 +21,13 @@ function displayNotification() {
 }
 
 function verifyRequiredFields() {
-  const name = nameInput.value.trim();
+  const firstName = firstNameInput.value.trim();
+  const lastName = lastNameInput.value.trim();
   const email = emailInput.value.trim();
   const password = passwordInput.value.trim();
   const passwordCheck = passwordInputCheck.value.trim();
 
-  if (!name || !email || !password || !passwordCheck) {
+  if (!lastName || !email || !password || !passwordCheck || firstName) {
     notificationTextElement.innerText = 'All fields are required. Please fill them all.'
     displayNotification();
   } else {
@@ -36,17 +39,17 @@ function verifyRequiredFields() {
         notificationTextElement.innerText = 'Unmatching password. Password needs to match for signup to be complete.'
         displayNotification();
       }
-      nameInput.value = '';
+      lastNameInput.value = '';
       passwordInput.value = '';
       passwordInputCheck.value = '';
-      return { name, email, password }
+      return { lastName, email, password }
     }
   }
 }
 
 signUpButton.addEventListener('click', () => {
-  const { email = '', password = '', name = '' } = verifyRequiredFields();
+  const { email = '', password = '', lastName = '' } = verifyRequiredFields();
   (email) ? sessionStorage.setItem('email', email) : '';
   (password) ? sessionStorage.setItem('password', password) : '';
-  if (email && password && name) return location.href = '../views/login.html';
+  if (email && password && lastName) return location.href = '../views/login.html';
 });
