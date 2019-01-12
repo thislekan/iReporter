89f2aec5eb2bf6926047707524323633e2ea105c
thislekan <lekanomoniyi@yahoo.com>
2019-01-12 02:45:34 +0100

    feature [modif-api]: fix error handling for uploads

--- server/middlewares/mediaMiddleware.js
+++ server/middlewares/mediaMiddleware.js
@@ -1,4 +1,29 @@
 /* eslint-disable no-console */
+import multer from 'multer';
+import cloudinary from 'cloudinary';
+import cloudinaryStorage from 'multer-storage-cloudinary';
+import dotEnv from '../config/config';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME || dotEnv.CLOUD_NAME,
+  api_key: process.env.API_KEY || dotEnv.API_KEY,
+  api_secret: process.env.API_SECRET || dotEnv.API_SECRET,
+});
+
+const storage = cloudinaryStorage({
+  cloudinary,
+  allowedFormats: ['jpg', 'svg', 'png', 'jpeg', 'gif', 'avi', 'flv', 'mpeg', '3gp', 'mp4'],
+  params: {
+    resource_type: 'auto',
+    folder: 'iReporter/media',
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 1000000 },
+}).any();
+
 const errorMessage = (res, status, message) => {
   res.status(status).send({
     status,
@@ -6,15 +31,16 @@ const errorMessage = (res, status, message) => {
   });
 };
 
-// function fileCheck(res, files) {
-//   files.forEach((element) => {
-//     if (!element.mimetype.includes('video') && !element.mimetype.includes('image')) {
-//       return errorMessage(res, 400, `${element.mimetype} format not supported.`);
-//     }
-//   });
-// }
-
 export default {
+  multerCheck: (req, res, next) => {
+    upload(req, res, (err) => {
+      if (err instanceof multer.MulterError) {
+        // A Multer error occurred when uploading.
+        return errorMessage(res, 400, err.message);
+      }
+      return next();
+    });
+  },
   returnedFiles: (req, res, next) => {
     if (!req.files.length) return next();
     const imageUrl = [];
--- server/routes/routesWithDb.js
+++ server/routes/routesWithDb.js
@@ -1,41 +1,14 @@
 /* eslint-disable no-console */
 import express from 'express';
-import multer from 'multer';
-import cloudinary from 'cloudinary';
-import cloudinaryStorage from 'multer-storage-cloudinary';
 import userHandler from '../controllers/dbUserHandler';
 import dbIncidentHandler from '../controllers/dbIncidentHandler';
 import incidentInputValidator from '../middlewares/incidentInputValidator';
 import userInputValidator from '../middlewares/userInputValidator';
 import authMiddleware from '../middlewares/authMiddleware';
 import mediaMiddleware from '../middlewares/mediaMiddleware';
-import dotEnv from '../config/config';
 
 const app = express.Router();
 const apiVersion = '/api/v2/';
-
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME || dotEnv.CLOUD_NAME,
-  api_key: process.env.API_KEY || dotEnv.API_KEY,
-  api_secret: process.env.API_SECRET || dotEnv.API_SECRET,
-});
-
-const storage = cloudinaryStorage({
-  cloudinary,
-  folder: 'iReporter/media',
-  allowedFormats: ['jpg', 'svg', 'png', 'jpeg', 'gif', 'avi', 'flv', 'mpeg', '3gp', 'mp4'],
-  // allowedFormats: (req, file, cb) => {
-  //   if (!file.mimetype.includes('image') && !file.mimetype.includes('vidoe')) {
-  //     return cb(new Error(`${file.mimetype} is not supported`));
-  //   }
-  //   return '';
-  // },
-});
-
-const upload = multer({
-  storage,
-  limits: 100000,
-});
 
 app
   .route(`${apiVersion}user/create`)
@@ -57,7 +30,7 @@ app
   .route(`${apiVersion}incident/create`)
   .post(
     authMiddleware.validateToken,
-    upload.any(),
+    mediaMiddleware.multerCheck,
     incidentInputValidator.createIncidentQueryValidator,
     mediaMiddleware.returnedFiles,
     dbIncidentHandler.createIncident,
